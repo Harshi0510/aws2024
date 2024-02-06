3a23dec289884621844728d2500a5b9f35422620
harshitha <harshithamunikoti0510@gmail.com>
2024-02-05 19:08:20 -0600

    harshitha python basic

---
+++ string.py
@@ -0,0 +1,11 @@
+# strings = https://docs.python.org/3/library/string.html
+#string methods = https://docs.python.org/3/library/string.html
+name = "harshitha"
+
+print(name)
+print(name.upper())
+print(name.title())
+
+first_name = ("harshitha")
+last_name = ("munikoti")
+print(f"{last_name} {first_name}")  #f.string = format string
---
+++ variable.py
@@ -0,0 +1,10 @@
+
+#variable Names:
+# - start with and alphabet
+# - Numbers and alphabets are allowed
+#https://docs.python.org/3/library/functions.html
+print("harshitha")
+_message = "harshitha"
+print(_message)
+student_name = "harshitha"
+
